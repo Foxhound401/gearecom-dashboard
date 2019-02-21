c3bbac2f24fbafaf0ec58e1534b8ced3f4b48d3b
brody <phungthienphuc18@gmail.com>
2019-02-21 11:47:36 +0700

    add test file

---
+++ src/views/faggit.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Container, Row, Col } from "shards-react";
+
+const FagitComponent = () =>{
+    <Container>
+        <Row>
+            Hello
+        </Row>
+    </Container>
+}
+
