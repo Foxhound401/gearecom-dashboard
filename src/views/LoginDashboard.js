f9346f78668a5735570c903ff32d868c759baf90
tonbao97 <30721039+tonbao97@users.noreply.github.com>
2019-03-12 14:21:19 +0700

    Login Page UI update

--- src/views/LoginDashboard.js
+++ src/views/LoginDashboard.js
@@ -8,8 +8,8 @@ import {
   Container,
   Button
 } from "shards-react";
-import { login } from '../components/api/UserFunction'
-
+import { login } from "../components/api/UserFunction";
+import BackgroundImage from "../images/background/background.png";
 //const LoginDashboard = () => (
 //  <Container>
 //    <Row style={loginComponent}>
@@ -44,10 +44,10 @@ class LoginDashboard extends Component {
     this.state = {
       email: "",
       password: ""
-    }
+    };
 
-    this.onChange = this.onChange.bind(this)
-    this.onSubmit = this.onSubmit.bind(this)
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
 
   onChange(e) {
@@ -60,53 +60,80 @@ class LoginDashboard extends Component {
     const user = {
       email: this.state.email,
       password: this.state.password
-    }
+    };
     login(user).then(res => {
       if (res) {
-        this.props.history.push(`/dashboard-overview`)
+        this.props.history.push(`/dashboard-overview`);
       }
-    })
+    });
   }
 
   render() {
     return (
-      <Container>
-        <Row style={loginComponent}>
-          <Col
-            xs={{ size: 8, order: 2, offset: 2 }}
-            sm={{ size: 7, order: 2, offset: 2 }}
-          >
-            <label>
-              <strong className="text-muted">Please sign in</strong>
-            </label>
-            <Form onSubmit={this.onSubmit}>
-              <label>Email</label>
-              <FormGroup>
-                <FormInput id="#email" name="email" placeholder="Email"  onChange={this.onChange} />
-              </FormGroup>
-              <label>Password</label>
-              <FormGroup>
-                <FormInput
-                  type="password"
-                  id="$password"
-                  placeholder="Password"
-                  name="password"
-                  onChange={this.onChange}
-                />
-              </FormGroup>
-              <Button outline pill size="md" type="submit">
-                Login to Dashboard
-              </Button>
-            </Form>
-          </Col>
-        </Row>
-      </Container>
+      <div style={Parent}>
+        <div style={InputSection}>
+          <Container>
+            <Row style={loginComponent}>
+              <Col
+                xs={{ size: 8, order: 2, offset: 2 }}
+                sm={{ size: 7, order: 2, offset: 2 }}
+              >
+                <label>
+                  <strong className="text-muted">Please sign in</strong>
+                </label>
+                <Form onSubmit={this.onSubmit}>
+                  <label>Email</label>
+                  <FormGroup>
+                    <FormInput
+                      id="#email"
+                      name="email"
+                      placeholder="Email"
+                      onChange={this.onChange}
+                    />
+                  </FormGroup>
+                  <label>Password</label>
+                  <FormGroup>
+                    <FormInput
+                      type="password"
+                      id="$password"
+                      placeholder="Password"
+                      name="password"
+                      onChange={this.onChange}
+                    />
+                  </FormGroup>
+                  <Button outline pill size="md" type="submit">
+                    Login to Dashboard
+                  </Button>
+                </Form>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+      </div>
     );
   }
 }
 
 const loginComponent = {
-  marginTop: "25%"
+  margin: "10px 10px 10px 10px"
 };
-
+const InputSection = {
+  backgroundColor: "#f2f2f2",
+  marginTop: "15%",
+  marginRight: "25%",
+  marginLeft: "25%",
+  border: "medium dashed #666666"
+};
+const Parent = {
+  position: "Fixed",
+  padding: 0,
+  margin: 0,
+  top: 0,
+  left: 0,
+  width: "100%",
+  height: "100%",
+  backgroundImage: `url(${BackgroundImage})`,
+  backgroundRepeat: "no-repeat",
+  backgroundSize: "cover"
+};
 export default LoginDashboard;
